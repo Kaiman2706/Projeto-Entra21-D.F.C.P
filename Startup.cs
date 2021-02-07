54428065289887d53c6fc14708264a75ce9e86c5
Kaiman2706 <74987683+Kaiman2706@users.noreply.github.com>
2021-02-07 14:11:43 -0300

    Update Context
    BD + Páginas em branco

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using DAL;
 
 namespace Projeto_Entra21_DFCP
 {
@@ -28,7 +29,7 @@ namespace Projeto_Entra21_DFCP
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
